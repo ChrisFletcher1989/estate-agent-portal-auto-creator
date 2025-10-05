d24434a16d2790e599461cd2dbe33d0a541b1185
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2025-10-05 17:59:23 +0900

    get dropbox link

--- src/dropbox/dropbox.module.ts
+++ src/dropbox/dropbox.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DropboxController } from './dropbox.controller';
 import { DropboxService } from './dropbox.service';
 
 @Module({
   imports: [],
-  controllers: [DropboxController],
   providers: [DropboxService],
 })
 export class DropboxModule {}
--- src/dropbox/dropbox.service.ts
+++ src/dropbox/dropbox.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Dropbox } from 'dropbox';
 
 @Injectable()
 export class DropboxService {
-  GetZip(): string {
-    return 'Hello World!';
+  private dropbox: Dropbox;
+
+  constructor(private configService: ConfigService) {
+    const accessToken = this.configService.get<string>('DROPBOX-ACCESS-TOKEN');
+    if (!accessToken) {
+      throw new Error(
+        'DROPBOX-ACCESS-TOKEN is not configured in environment variables',
+      );
+    }
+
+    this.dropbox = new Dropbox({
+      accessToken: accessToken,
+      fetch: fetch,
+    });
+  }
+
+  async getTempLink(path: string): Promise<string> {
+    try {
+      const response = await this.dropbox.filesGetTemporaryLink({ path });
+      return response.result.link;
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error occurred';
+      throw new Error(`Failed to get temporary link: ${errorMessage}`);
+    }
   }
 }
